cafdbec54296c73ba8d76a0ec0aee1847549b4ac
oleksandrashevchenko98 <oleksandrashevchenko98@gmail.com>
2024-03-04 13:20:47 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+
+const { dbConnect } = require('./config/db')
+
+const logger = (logType, ...args) => {
+    const allowedTypes = {
+        local: ['Emergency', 'Alert', 'Critical', 'Error', 'Warning', 'Notice', 'Informational', 'Debug'],
+        development: ['Emergency', 'Alert', 'Critical', 'Error', 'Warning', 'Notice'],
+        production: ['Emergency', 'Alert', 'Critical', 'Error'],
+    };
+
+    if (!Object.keys(allowedTypes).includes(process.env.NODE_ENV)) {
+        console.error('Invalid NODE_ENV value');
+        return;
+    }
+
+    if (!allowedTypes[process.env.NODE_ENV].includes(logType)) {
+        console.error(`Invalid logType for NODE_ENV ${process.env.NODE_ENV}`);
+        return;
+    }
+    switch (logType) {
+        case 'Emergency':
+        case 'Alert':
+        case 'Critical':
+        case 'Error':
+            console.error(`[${logType}] [${process.env.NODE_ENV}]`, ...args);
+            break;
+        case 'Warning':
+        case 'Notice':
+        case 'Informational':
+        case 'Debug':
+            console.log(`[${logType}] [${process.env.NODE_ENV}]`, ...args);
+            break;
+        default:
+            console.error(`Unsupported logType: ${logType}`);
+    }
+};
+
+logger('Debug', { a: 1, b: 2 });
+
+try {
+    fnCal();
+    // Error
+} catch (err) {
+    logger('Error', err.name);
+}
---
+++ config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+    dbConnect: {
+        dbUser: process.env['DB_USER'],
+        dbPass: process.env['DB_PASS'],
+        dbHost: process.env['DB_HOST']
+    }
+}
